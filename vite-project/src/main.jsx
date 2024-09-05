3e9f3efb2a98235f38d6b64b424b645652e7a78c
farahzet <119497241+farahzet@users.noreply.github.com>
2024-09-06 05:35:27 +0700

    update sinpeet

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -7,26 +7,13 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 
-// Memasang kode Maze dengan benar
-(function(w, d) {
-  var s, t;
-  var scriptElement = d.createElement('script');  // Buat elemen script baru
-  scriptElement.src = 'https://cdn.maze.co/maze.js';
-  scriptElement.async = true;
-  
-  s = d.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(scriptElement, s);
-  
-  t = d.createElement('noscript');
-  t.innerHTML = '<img src="https://cdn.maze.co/track.gif" />';
-  d.getElementsByTagName('body')[0].appendChild(t);
-})(window, document);
 
-// Render aplikasi React dengan benar
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+
+    {/* <App/> */}
+  </React.StrictMode>,
+)
