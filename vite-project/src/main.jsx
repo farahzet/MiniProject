fc33b419bfe1b53f0ab9ceb8cd4a68ed47072f0e
farahzet <119497241+farahzet@users.noreply.github.com>
2024-09-06 05:14:07 +0700

    add code spinnet

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -6,6 +6,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
+(function(m,a,z,e){
+  var s,t;o=a.createElement('script');o.src='https://cdn.maze.co/maze.js';o.async=true;
+  s=a.getElementsByTagName('script')[0];s.parentNode.insertBefore(o,s);
+  t=m.createElement('noscript');t.innerHTML='<img src="https://cdn.maze.co/track.gif" />';
+  a.getElementsByTagName('body')[0].appendChild(t);
+})(window,document);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
