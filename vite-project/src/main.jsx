d6666be4e00fcb6f0fd9d63f8063c9988cc50cf9
farahzet <119497241+farahzet@users.noreply.github.com>
2024-09-06 05:47:53 +0700

    update main

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './index.css'
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-
-    {/* <App/> */}
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
